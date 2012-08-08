4648cacdbc485f6f9edbc92e90c6205d469d0b2e
Alex <ajbahouth@gmail.com>
2012-08-08 16:06:22 -0700

    Initial checkin of base files

---
+++ src/roxxi/utils/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-common-utils.core)
---
+++ test/roxxi/utils/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-common-utils.test.core
+  (:use [clojure-common-utils.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
