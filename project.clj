2740dfb3cc36b88c1aa8b624b3349371e872406d
Alex <ajbahouth@gmail.com>
2012-08-08 17:07:40 -0700

    fixed white space

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "This is a library location for me plunk a lot of common utilities I want to keep available"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :license {:name "Eclipse Public License - v 1.0"
-                     :url "http://www.eclipse.org/legal/epl-v10.html"
-                      :distribution :repo
-                                  :comments "same as Clojure"}
-                                  :url https://github.com/Roxxi/clojure-common-utils
-)
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :url https://github.com/Roxxi/clojure-common-utils
+  )
