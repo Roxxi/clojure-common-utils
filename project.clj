9086f31f5e6b12712d2663b196c73d309c72a5f3
Alex <ajbahouth@gmail.com>
2012-08-20 16:30:26 -0700

    Posted the first version to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject roxxi/clojure-common-utils "0.0.1-SNAPSHOT"
+(defproject roxxi/clojure-common-utils "0.0.1"
   :description "This is a library location for me plunk a lot of common utilities I want to keep available"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
