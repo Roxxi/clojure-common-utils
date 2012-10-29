088cf70aff7ee58bb369ea4de63757aabd47aacc
Alex <ajbahouth@gmail.com>
2012-10-29 16:27:19 -0700

    Looks like you can't just throw around realized? It's a shame that "unrealized?" and "realizable?" don't exist :P

--- src/roxxi/utils/print.clj
+++ src/roxxi/utils/print.clj
@@ -5,10 +5,21 @@
      (cond (nil? e#)
            (println
             (str "Expression " '~e " evaluates to nil\n+\n+")),
-       (and (seq? e#) (not (realized? e#)))
-       (println
-        (str "Expression " '~e " evaluates to " (vec e#) " of type " (.getClass #^Object e#) "\n+\n+"))
-       :else
-       (println
-        (str "Expression " '~e " evaluates to " e# " of type " (.getClass #^Object e#) "\n+\n+")))
+           (and (seq? e#))
+           (println
+            (str "Expression " '~e " evaluates to " (vec e#) " of type " (.getClass #^Object e#) "\n+\n+")),
+           :else
+           (println
+            (str "Expression " '~e " evaluates to " e# " of type " (.getClass #^Object e#) "\n+\n+")))
+     e#))
+
+;; for primitives without type hinting
+(defmacro print-prim [e]
+  `(let [e# ~e]
+     (cond (nil? e#)
+           (println
+            (str "Expression " '~e " evaluates to nil\n+\n+")),
+           :else
+           (println
+            (str "Expression " '~e " evaluates to " e# "\n+\n+")))
      e#))
