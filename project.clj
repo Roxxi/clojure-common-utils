1567beff12dcfaaa352fa9759df83c739df035c3
Alex Bahouth <abahouth@onekingslane.com>
2013-02-12 18:51:59 -0800

    Back to Snapshot status

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject roxxi/clojure-common-utils "0.0.4"
+(defproject roxxi/clojure-common-utils "0.0.5-SNAPSHOT"
   :description "This is a library location for me plunk a lot of common utilities I want to keep available for use indepent of the project"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :license {:name "Eclipse Public License - v 1.0"
