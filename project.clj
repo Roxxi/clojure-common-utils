6f54358947d603540056a5ae8c8ec5b6076fb523
Alex Bahouth <roxxibear@gmail.com>
2013-07-05 12:40:45 -0700

    Changed doall back to map- doall returns a sequence, where as vec forces the sequence to be realized into a printable format of a vector.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject roxxi/clojure-common-utils "0.0.10"
+(defproject roxxi/clojure-common-utils "0.0.11"
   :description "This is a library location for me plunk a lot of common utilities I want to keep available for use indepent of the project"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :url https://github.com/Roxxi/clojure-common-utils
-  )
+  :url https://github.com/Roxxi/clojure-common-utils)
