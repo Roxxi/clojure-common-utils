a846f210d2e6d90fc89d47bf3c62e72e45dd3f88
Matt Halverson <mhalverson@onekingslane.com>
2014-02-10 14:23:47 -0800

    Added print-expr-rec

--- src/roxxi/utils/print.clj
+++ src/roxxi/utils/print.clj
@@ -13,6 +13,20 @@
             (str "Expression " '~e " evaluates to " e# " of type " (.getClass #^Object e#) "\n+\n+")))
      e#))
 
+(defn- process-form-recursive [form macro-name & {:keys [hella]
+                                                  :or {hella false}}]
+  (cond
+   (list? form)
+   `(~macro-name ~(map #(process-form-recursive % macro-name :hella hella) form))
+   hella
+   `(~macro-name ~form)
+   :else
+   form))
+(defmacro print-expr-rec [form]
+  (process-form-recursive form 'print-expr))
+(defmacro print-expr-hella-rec [form]
+  (process-form-recursive form 'print-expr :hella true))
+
 ;; for primitives without type hinting
 (defmacro print-prim [e]
   `(let [e# ~e]
@@ -27,7 +41,3 @@
 
 (defmacro multi-line-str [& strs]
   (clojure.string/join " " strs))
-
-
-        
-
--- test/roxxi/utils/print_test.clj
+++ test/roxxi/utils/print_test.clj
@@ -9,3 +9,10 @@
                          "i am fine")
          "hello how are you? i am fine")))
 
+(deftest print-expr-rec-test []
+  (testing "Does the correct string get written to std-out"
+    (let [pythag (fn [x y]
+                   (print-expr-rec (+ (* x x) (* y y))))
+          what-got-written-to-output (with-out-str (pythag 3 4))]
+      (is (= what-got-written-to-output
+             "Expression (* x x) evaluates to 9 of type class java.lang.Long\n+\n+\nExpression (* y y) evaluates to 16 of type class java.lang.Long\n+\n+\nExpression (+ (print-expr (* x x)) (print-expr (* y y))) evaluates to 25 of type class java.lang.Long\n+\n+\n")))))
